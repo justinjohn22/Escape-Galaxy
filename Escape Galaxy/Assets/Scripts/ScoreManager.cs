3444cea74226a95f5a398f5646f92b56821ab7fc
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-06-21 23:42:04 +1000

    started working on score

---
+++ Escape Galaxy/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Star"))
+        {
+            score++;
+            Debug.Log(score);
+        }
+    }
+}
