693e14cd6778ff4045a425beb7d2a68b937ae509
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-02 14:05:27 +1000

    fps update

--- Escape Galaxy/Assets/Scripts/Quality/TargetFrameRate.cs
+++ Escape Galaxy/Assets/Scripts/Quality/TargetFrameRate.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetFrameRate : MonoBehaviour
-{   
+{
+    public int targetFPS;
+
     // Start is called before the first frame update
     void Start()
     {
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = targetFPS;
     }
     // Update is called once per frame
     void Update()
