ae8152a0555ede35de1e36dd714e9fa2ed37b799
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-04 14:25:44 +1000

    added fuel

--- Escape Galaxy/Assets/Scripts/Player.cs
+++ Escape Galaxy/Assets/Scripts/Player.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Player : MonoBehaviour
 {
     public float playerSpeed;
     public int health;
+    public float fuel;
+    public float fuelDecrement;
     public Text healthDisplay;
- 
+    public Text fuelDisplay;
+
 
     private Shake shake;
     private bool movingLeft;
@@ -35,14 +39,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         healthDisplay.text = health.ToString();
+        fuelDisplay.text = Math.Round(fuel).ToString();
 
+        fuel -= fuelDecrement;
+     
         if (transform.position.y < 2)
         {
             shake.CamShake();
             transform.Translate(Vector2.up * Time.deltaTime * boost);
         }
 
-        if (health < 0)
+        if (health < 0 || fuel <= 0)
         {
             SceneManager.LoadScene("GameOver");
         }
@@ -68,18 +75,18 @@ public class Player : MonoBehaviour
  
             if (movingLeft && startTime > 0.1f)
             {
-                transform.Translate(Vector2.left * Time.deltaTime * playerSpeed * 1.5f);
+                transform.Translate(Vector2.left * Time.deltaTime * playerSpeed * 1.255f);
             } 
             else if (!movingLeft && startTime > 0.1f)
             {
-                transform.Translate(Vector2.right * Time.deltaTime * playerSpeed * 1.5f);   
+                transform.Translate(Vector2.right * Time.deltaTime * playerSpeed * 1.255f);   
             }
        
-            else if (movingLeft && !sideBoost)
+            else if (movingLeft)
             {
                 transform.Translate(Vector2.left * Time.deltaTime * playerSpeed);
             }
-            else if (!movingLeft && !sideBoost)
+            else if (!movingLeft)
             {
                 transform.Translate(Vector2.right * Time.deltaTime * playerSpeed);
             }
