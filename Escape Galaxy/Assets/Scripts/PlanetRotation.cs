4c06b895c7ecbe11e850bc1a2ac860d2ea1cea24
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-05 19:08:05 +1000

    randomized obstacle rotation

--- Escape Galaxy/Assets/Scripts/PlanetRotation.cs
+++ Escape Galaxy/Assets/Scripts/PlanetRotation.cs
@@ -6,8 +6,22 @@ public class PlanetRotation : MonoBehaviour
 {
     // Update is called once per frame
     public float spinValue;
+
+    private int randInt;
+
+    void Start()
+    {
+        randInt = Random.Range(-1, 1);
+
+        if (randInt == 0)
+        {
+            randInt++;
+        }
+    }
     void Update()
     {
-        transform.Rotate(0, 0, spinValue * Time.deltaTime);
+        
+
+        transform.Rotate(0, 0, spinValue * Time.deltaTime * randInt);
     }
 }
