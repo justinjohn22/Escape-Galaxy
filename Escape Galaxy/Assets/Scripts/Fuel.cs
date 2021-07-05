486c063a1395f9204e6e65608e6a933d574cc9fe
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-05 16:04:24 +1000

    added fuel increment feature

---
+++ Escape Galaxy/Assets/Scripts/Fuel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuel : MonoBehaviour
+{
+    public float fuelIncrement;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {   
+            if (other.GetComponent<Player>().fuel <= 95)
+            {
+                other.GetComponent<Player>().fuel += fuelIncrement;
+            } 
+            else if (other.GetComponent<Player>().fuel >= 95)
+            {
+                other.GetComponent<Player>().fuel = 100;
+            }
+           
+        }
+    }
+}
