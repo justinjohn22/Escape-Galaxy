30a08a7816b7ebf554173a1b8c2e445e43247e40
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-08 23:39:57 +1000

    Coin adding animation feature

--- Escape Galaxy/Assets/Scripts/Game Menu/CurrentCoin.cs
+++ Escape Galaxy/Assets/Scripts/Game Menu/CurrentCoin.cs
@@ -11,19 +11,56 @@ public class CurrentCoin : MonoBehaviour
     public Text totalCoin;
 
     private int RESET_VALUE = 0;
+    private float startTime;
+    private int score, coin, coinCollected;
 
     // Start is called before the first frame update
     void Start()
     {   
+        // getting values from player prefs
+        score = PlayerPrefs.GetInt("TempScore");
+        coin = PlayerPrefs.GetInt("Coin") - PlayerPrefs.GetInt("CurrentCoin");
+        
+        Debug.Log("coin: " + coin);
+        coinCollected = PlayerPrefs.GetInt("CurrentCoin");
+  
         // display the most resent score and coins collected by the player 
         coinsCollected.text = "+" + PlayerPrefs.GetInt("CurrentCoin").ToString();
         currentScore.text = PlayerPrefs.GetInt("TempScore").ToString();
-        totalCoin.text = PlayerPrefs.GetInt("Coin").ToString();
 
+        // if current coin is zero, update if statement won't be called
+        if (coinCollected == 0)
+        {
+            totalCoin.text = PlayerPrefs.GetInt("Coin").ToString();
+        }
+       
+        // reset player prefs after values are stored
+        ResetPlayerPrefs();
+    }
+
+    void Update()
+    {   
+        // to animation coins being added up in game over scene
+        startTime = Time.time;
+        if (startTime > 10.0f) // to slow to addition and substraction
+        {
+            if (coin < coin + coinCollected)
+            {   
+                // play the adding animation
+                coin++;
+                totalCoin.text = coin.ToString(); // display to screen
+                coinCollected--;
+                startTime = 0f;
+            }
+        }
+
+    }
+
+    private void ResetPlayerPrefs()
+    {
         // reset values to be used again
         PlayerPrefs.SetInt("CurrentCoin", RESET_VALUE);
         PlayerPrefs.SetInt("TempScore", RESET_VALUE);
-        
     }
 
 }
