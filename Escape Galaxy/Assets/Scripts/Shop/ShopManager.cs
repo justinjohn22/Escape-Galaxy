7af79f1765406c1ec737a821f7e2e224b1ec9811
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-24 19:47:36 +1000

    added documentation and improved shop ui

--- Escape Galaxy/Assets/Scripts/Shop/ShopManager.cs
+++ Escape Galaxy/Assets/Scripts/Shop/ShopManager.cs
@@ -10,12 +10,21 @@ public class ShopManager : MonoBehaviour
     public Text shipTwoText;
     public Text shipThreeText;
 
+    public Text totalCoins;
+
     void Start()
-    {
+    {   
+        // initial text setup for selected ship
         Text[] textArray = new Text[] { shipOneText, shipTwoText, shipThreeText };
         textArray[PlayerPrefs.GetInt("SelectedPlayer")].text = "SELECTED";
     }
 
+    void Update() 
+    {
+        totalCoins.text = PlayerPrefs.GetInt("Coin").ToString();
+    }
+    
+    // shop logic for ship 1
     public void shipOne()
     {
         PlayerPrefs.SetInt("SelectedPlayer", 0);
@@ -25,6 +34,7 @@ public class ShopManager : MonoBehaviour
         // Debug.Log("Ship 0");
     }
 
+    // shop logic for ship 2
     public void shipTwo()
     {
         PlayerPrefs.SetInt("SelectedPlayer", 1);
@@ -34,6 +44,7 @@ public class ShopManager : MonoBehaviour
         // Debug.Log("Ship 1");
     }
 
+    // shop logic for ship 3
     public void shipThree()
     {
         PlayerPrefs.SetInt("SelectedPlayer", 2);
@@ -43,7 +54,7 @@ public class ShopManager : MonoBehaviour
         // Debug.Log("Ship 2");
     }
 
-  
+    // resets all other ship text back to 'select' once a ship is 'selected'
     private void ResetText(int index)
     {
         Text[] textArray = new Text[] { shipOneText, shipTwoText, shipThreeText };
@@ -57,6 +68,7 @@ public class ShopManager : MonoBehaviour
         }
     }
 
+    // automatic game scene load after a ship is selected
     private void LoadGameScene()
     {
         SceneManager.LoadScene("GameScene");
