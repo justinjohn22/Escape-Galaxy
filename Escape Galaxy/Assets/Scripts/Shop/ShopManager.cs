0ad3fd260f5bf27a52bdb2a4070812ac9d9df899
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-25 17:03:57 +1000

    added shop currency logic

--- Escape Galaxy/Assets/Scripts/Shop/ShopManager.cs
+++ Escape Galaxy/Assets/Scripts/Shop/ShopManager.cs
@@ -12,11 +12,33 @@ public class ShopManager : MonoBehaviour
 
     public Text totalCoins;
 
+
+    private int SHIP_TWO_COST = 550;
+    private int SHIP_THREE_COST = 850;
+
     void Start()
-    {   
+    {
+        // default ship 
+        PlayerPrefs.SetInt("ShipOne", 1);
+
         // initial text setup for selected ship
         Text[] textArray = new Text[] { shipOneText, shipTwoText, shipThreeText };
+        string[] savedList = new string[] { "ShipOne", "ShipTwo", "ShipThree" };
+
+
         textArray[PlayerPrefs.GetInt("SelectedPlayer")].text = "SELECTED";
+
+        for (int i = 0; i < 3; ++i)
+        {   
+            if (PlayerPrefs.GetInt(savedList[i]) == 0)
+            {
+                textArray[i].text = "BUY";
+
+            } else if (PlayerPrefs.GetInt(savedList[i]) == 1 && PlayerPrefs.GetInt("SelectedPlayer") != i)
+            {
+                textArray[i].text = "SELECT";
+            }
+        }
     }
 
     void Update() 
@@ -37,31 +59,58 @@ public class ShopManager : MonoBehaviour
     // shop logic for ship 2
     public void shipTwo()
     {
-        PlayerPrefs.SetInt("SelectedPlayer", 1);
-        shipTwoText.text = "SELECTED";
-        ResetText(1);
-        LoadGameScene();
+        if (PlayerPrefs.GetInt("ShipTwo", 0) == 1)
+        {
+            PlayerPrefs.SetInt("SelectedPlayer", 1);
+            shipTwoText.text = "SELECTED";
+            ResetText(1);
+            LoadGameScene();
+        } 
+        else if (PlayerPrefs.GetInt("ShipTwo", 0) == 0)
+        {
+            if (PlayerPrefs.GetInt("Coin") - SHIP_TWO_COST >= 0)
+            {
+                PlayerPrefs.SetInt("Coin", PlayerPrefs.GetInt("Coin") - SHIP_TWO_COST);
+                PlayerPrefs.SetInt("ShipTwo", 1);
+                shipTwoText.text = "SELECT";
+            }
+        }
+           
         // Debug.Log("Ship 1");
     }
 
     // shop logic for ship 3
     public void shipThree()
-    {
-        PlayerPrefs.SetInt("SelectedPlayer", 2);
-        shipThreeText.text = "SELECTED";
-        ResetText(2);
-        LoadGameScene();
+    { 
         // Debug.Log("Ship 2");
+
+        if (PlayerPrefs.GetInt("ShipThree", 0) == 1)
+        {
+            PlayerPrefs.SetInt("SelectedPlayer", 2);
+            shipThreeText.text = "SELECTED";
+            ResetText(2);
+            LoadGameScene();
+        }
+        else if (PlayerPrefs.GetInt("ShipThree", 0) == 0)
+        {
+            if (PlayerPrefs.GetInt("Coin") - SHIP_THREE_COST >= 0)
+            {
+                PlayerPrefs.SetInt("Coin", PlayerPrefs.GetInt("Coin") - SHIP_TWO_COST);
+                PlayerPrefs.SetInt("ShipThree", 1);
+                shipThreeText.text = "SELECT";
+            }
+        }
     }
 
     // resets all other ship text back to 'select' once a ship is 'selected'
     private void ResetText(int index)
     {
         Text[] textArray = new Text[] { shipOneText, shipTwoText, shipThreeText };
-       
+        string[] savedList = new string[] { "ShipOne", "ShipTwo", "ShipThree" };
+
         for (int i = 0; i < 3; ++i)
         {
-            if (i != index)
+            if (i != index && PlayerPrefs.GetInt(savedList[i]) == 1)
             {
                 textArray[i].text = "SELECT";
             }
