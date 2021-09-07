80f82464c485b77b408595c7ed8d8e7d560d5a74
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-09-07 12:25:59 +1000

    shop aspect ratio fixes

---
+++ Escape Galaxy/Assets/Scripts/Shop/PositionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionManager : MonoBehaviour
+{
+
+    public GameObject shopPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // x = 4064
+        // Vector3 pos = new Vector3(-10f, 0, 0);
+        // shopPanel.transform.position += pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
